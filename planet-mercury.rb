0209ab8abbfaa5821861ce1e07695f8408a703f8
Keith Gaughan <k@stereochro.me>
2025-06-07 23:11:26 +0100

    Brew formula update for mercury version v0.4.8

--- planet-mercury.rb
+++ planet-mercury.rb
@@ -5,21 +5,21 @@
 class PlanetMercury < Formula
   desc "A Planet-style feed aggregator"
   homepage "https://github.com/kgaughan/mercury"
-  version "0.4.7"
+  version "0.4.8"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/kgaughan/mercury/releases/download/v0.4.7/mercury_0.4.7_darwin_x86_64.tar.gz"
-      sha256 "93cf5c486bba5a2488011cd43c1302f415df198463bda56c3aa522db5fc7a55d"
+      url "https://github.com/kgaughan/mercury/releases/download/v0.4.8/mercury_0.4.8_darwin_x86_64.tar.gz"
+      sha256 "9177366fa6c3c992b7a377bd5bc39802cc4bdc11fa0f14cc18fdcd4f3ddcbdff"
 
       def install
         bin.install "mercury"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/kgaughan/mercury/releases/download/v0.4.7/mercury_0.4.7_darwin_arm64.tar.gz"
-      sha256 "d97c0f1f05b4cdd6a149a56c8ba39f3264b3aef6bef95ee878edcb347fb9c1e1"
+      url "https://github.com/kgaughan/mercury/releases/download/v0.4.8/mercury_0.4.8_darwin_arm64.tar.gz"
+      sha256 "1894941f5e45bfce7cf195bc69499262a336f7904009adbe052c1f6645dae854"
 
       def install
         bin.install "mercury"
@@ -28,24 +28,18 @@ class PlanetMercury < Formula
   end
 
   on_linux do
-    if Hardware::CPU.intel?
-      if Hardware::CPU.is_64_bit?
-        url "https://github.com/kgaughan/mercury/releases/download/v0.4.7/mercury_0.4.7_linux_x86_64.tar.gz"
-        sha256 "88561d0a1b2f3e9b3250cf45251cb961db177a317a21b94b628b1692f8016f77"
-
-        def install
-          bin.install "mercury"
-        end
+    if Hardware::CPU.intel? and Hardware::CPU.is_64_bit?
+      url "https://github.com/kgaughan/mercury/releases/download/v0.4.8/mercury_0.4.8_linux_x86_64.tar.gz"
+      sha256 "a7f3f5e413d0253df03690e4c486464ee38e57ff109bf7df5ca066757b94c526"
+      def install
+        bin.install "mercury"
       end
     end
-    if Hardware::CPU.arm?
-      if Hardware::CPU.is_64_bit?
-        url "https://github.com/kgaughan/mercury/releases/download/v0.4.7/mercury_0.4.7_linux_arm64.tar.gz"
-        sha256 "d102bfb437eb0f2f84e1b2de1c2f8313bfe6a50978128b296051f44914c19d88"
-
-        def install
-          bin.install "mercury"
-        end
+    if Hardware::CPU.arm? and Hardware::CPU.is_64_bit?
+      url "https://github.com/kgaughan/mercury/releases/download/v0.4.8/mercury_0.4.8_linux_arm64.tar.gz"
+      sha256 "93f55d3dc4d2d7bc07cdc221777807a05b6695130a89a4cd2fe0bd180b56ed90"
+      def install
+        bin.install "mercury"
       end
     end
   end
